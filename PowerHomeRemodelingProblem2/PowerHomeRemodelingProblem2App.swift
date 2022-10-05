011968b84aa1cff88ea01a4c07c2aa4b6fbe319c
Eric Wroblewski <ericwroble@gmail.com>
2022-10-05 12:33:03 -0400

    Initial Commit

---
+++ PowerHomeRemodelingProblem2/PowerHomeRemodelingProblem2App.swift
@@ -0,0 +1,17 @@
+//
+//  PowerHomeRemodelingProblem2App.swift
+//  PowerHomeRemodelingProblem2
+//
+//  Created by Eric Wroblewski on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PowerHomeRemodelingProblem2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
